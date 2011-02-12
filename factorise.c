c5af5b42e47de6c3104c2b50ac23e3cbc87615e7
Kiprianas Spiridonovas <neegzistuoju@gmail.com>
2011-02-12 22:51:35 +0200

    Order includes.

--- factorise.c
+++ factorise.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include <errno.h>
 #include <limits.h>
-#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
